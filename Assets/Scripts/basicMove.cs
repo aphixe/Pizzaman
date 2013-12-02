3bd00bb9e91d8b4d7f6614336b522594644f5f7e
aphixe <aphixe@gmail.com>
2013-12-01 22:56:08 -0800

    Move Script Improvements
    the script for moving player nearly appears to be working as wanted,
    there is issues with evil physics still needing work

---
+++ Assets/Scripts/basicMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicMove : MonoBehaviour {
+
+	public KeyCode moveLeft;
+	public KeyCode moveRight;
+	public float speed = 10;
+	float rigidValue;
+
+	void Start()
+	{
+		float rigidValue = rigidbody2D.velocity.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (moveLeft)) 
+		{
+			rigidValue = speed;
+		}   
+		else if (Input.GetKey (moveRight))
+		{	
+			rigidValue = speed * -1;
+		}
+	}
+}
---
+++ Assets/Scripts/simpleMove.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var moveLeft: KeyCode;
+var moveRight: KeyCode;
+
+var moveUp: KeyCode;
+var moveDown: KeyCode;
+
+var speed : float = 10;
+
+function Update () {
+	if (Input.GetKey(moveLeft))
+	{
+		rigidbody2D.velocity.x = speed * -1;
+	}
+	else if(Input.GetKey(moveRight))	
+	{
+		rigidbody2D.velocity.x = speed;
+	}
+	else if(Input.GetKey(moveUp))	
+	{
+		rigidbody2D.velocity.y = speed;
+	}
+	else if(Input.GetKey(moveDown))	
+	{
+		rigidbody2D.velocity.y = speed * -1;
+	}
+
+}
